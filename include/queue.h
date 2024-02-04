6ccee8d1c33787fd524bf31444c1f89868f11fd5
DarkSharpness <2040703891@qq.com>
2024-02-04 19:11:40 +0800

    Upd(queue): Add a interesting toy queue.

---
+++ include/queue.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <queue>
+#include <unordered_set>
+
+namespace dark {
+
+template <typename _Tp>
+struct work_queue : public std::queue <_Tp> , protected std::unordered_set <_Tp> {
+  private:
+    using _Set_t        = std::unordered_set <_Tp>;
+    using _List_t       = std::queue <_Tp>;
+    using _Container_t  = typename _List_t::container_type;
+
+  public:
+    /* Return the underlying container. */
+    _Container_t & container() { return _List_t::c; }
+    /* Add an element to the queue if it is not already set. */
+    void push(const _Tp & __t) { if (this->insert(__t).second) _List_t::push(__t); }
+    /* Add an element to the queue if it is not already set. */
+    void push(_Tp && __t) { if (this->insert(__t).second) _List_t::push(std::move(__t)); }
+    /* Force to push an element to the queue. */
+    void force_push(_Tp __t) { this->insert(__t); _List_t::push(std::move(__t)); } 
+    /* Pop the first element and return it. */
+    [[nodiscard]]_Tp pop_value() {
+        _Tp __t = std::move(_List_t::front()); _List_t::pop();
+        return __t;
+    }
+    std::size_t size() const { return _List_t::size(); }
+    bool empty() const { return _List_t::empty(); }
+};
+
+
+} // namespace dark
