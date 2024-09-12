fb01d9d9cf119a69629a4743451d13cfd92e624a
DarkSharpness <2040703891@qq.com>
2024-09-12 14:52:51 +0800

    refactor: add some control to the optimizer

--- src/OPT/optimizer.cpp
+++ src/OPT/optimizer.cpp
@@ -1,9 +1,8 @@
 #include "optimizer.h"
 #include "AST/folder.h"
-#include "IRnode.h"
+#include "IRbase.h"
 #include "IRbuilder.h"
 #include "dominant.h"
-#include "CFGbuilder.h"
 #include "MEM2REG/mem2reg.h"
 #include "DCE/unreachable.h"
 #include "DCE/dce.h"
@@ -14,6 +13,8 @@
 #include "CM/gcm.h"
 #include "EARLY/gcr.h"
 #include "CFGsimplifier.h"
+#include <string_view>
+#include <type_traits>
 
 namespace dark {
 
@@ -27,36 +28,50 @@ static void DoNotOptimize(IR::IRbuilder *ctx) {
     }
 }
 
+template<typename _Pass, typename _Tp>
+static void optimize(_Tp &object) {
+    if constexpr (requires { _Pass { object }; }) {
+        static_cast<void>(_Pass { object });
+    } else {
+        static_cast<void>(_Pass { &object });
+    }
+}
+
 static void DoOptimize(IR::IRbuilder *ctx) {
     DoNotOptimize(ctx); // First, remove unreachable code.
-    IR::GlobalConstantReplacer { ctx };
+
+    optimize<IR::GlobalConstantReplacer>(ctx);
 
     auto &functions = ctx->global_functions;
     for (auto &__func : functions) {
-        IR::mem2regPass { &__func };
-        IR::DeadCodeEliminator { &__func };
-        IR::ConstantPropagatior { &__func };
-        IR::dominantMaker::clean(&__func);
-        IR::unreachableRemover { &__func };
-        IR::AggressiveElimination { &__func };
-        IR::CFGsimplifier { &__func };
+        optimize<IR::mem2regPass>(__func);
+        optimize<IR::DeadCodeEliminator>(__func);
+        optimize<IR::ConstantPropagatior>(__func);
+        optimize<IR::dominantMaker>(__func);
+        optimize<IR::unreachableRemover>(__func);
+        optimize<IR::AggressiveElimination>(__func);
+        optimize<IR::CFGsimplifier>(__func);
 
-        IR::unreachableRemover { &__func };
-        IR::DeadCodeEliminator { &__func };
-        IR::dominantMaker { &__func };
-        IR::ConstantPropagatior { &__func };
-        IR::unreachableRemover { &__func };
-        IR::AggressiveElimination { &__func };
-        IR::CFGsimplifier { &__func };
+        optimize<IR::unreachableRemover>(__func);
+        optimize<IR::unreachableRemover>(__func);
+        optimize<IR::DeadCodeEliminator>(__func);
+        optimize<IR::dominantMaker>(__func);
+        optimize<IR::ConstantPropagatior>(__func);
+        optimize<IR::unreachableRemover>(__func);
+        optimize<IR::AggressiveElimination>(__func);
+        optimize<IR::CFGsimplifier>(__func);
 
-        IR::LoopNestDetector { &__func };
-        IR::GlobalCodeMotionPass { &__func };
-        IR::GlobalValueNumberPass { &__func };
-        IR::DeadCodeEliminator { &__func };
-        IR::ConstantPropagatior { &__func };
-        IR::unreachableRemover { &__func };
-        IR::DeadCodeEliminator { &__func };
+        optimize<IR::LoopNestDetector>(__func);
+        optimize<IR::GlobalCodeMotionPass>(__func);
+        optimize<IR::GlobalValueNumberPass>(__func);
+        optimize<IR::DeadCodeEliminator>(__func);
+        optimize<IR::ConstantPropagatior>(__func);
+        optimize<IR::unreachableRemover>(__func);
+        optimize<IR::DeadCodeEliminator>(__func);
     }
+
+    optimize<IR::GlobalConstantReplacer>(ctx);
+
 }
 
 void optimizer::optimize_AST(AST::ASTbuilder *ctx) {
@@ -69,12 +84,21 @@ void optimizer::optimize_IR(IR::IRbuilder *ctx) {
     std::cout << ctx->IRtree();
 }
 
-
 void optimizer::init(int argc, char **argv) {
-
-
+    if (argc != 0) {
+        for (int i = 1 ; i < argc ; ++i) {
+            const auto str = std::string_view { argv[i] };
+            if (str == "-O2") {
+                optimize_info.level = 2;
+            } else if (str == "-O1") {
+                optimize_info.level = 1;
+            } else if (str == "-O0") {
+                optimize_info.level = 0;
+            } else {
+                std::cerr << "Unknown option: " << str << std::endl;
+            }
+        }
+    }
 }
-
-
 
 } // namespace dark
